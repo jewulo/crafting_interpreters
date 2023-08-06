822f5756ac96f84f46f4604087c0e4ef4805eea1
Joshua Ewulo <jewulo@gmail.com>
2023-08-07 00:25:03 +0100

    Initial commit

---
+++ src/crafting_interpreters/lox/chapter_4/Token.java
@@ -0,0 +1,19 @@
+package crafting_interpreters.lox;
+
+public class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String tostring() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
