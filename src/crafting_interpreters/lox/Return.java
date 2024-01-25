105bfd7ca8d50d5db13e54796a2e5df3bbd51a70
Joshua Ewulo <jewulo@gmail.com>
2024-01-25 01:07:49 +0000

    Completed Chapter 12. Objects now have a constructor called init().

--- src/crafting_interpreters/lox/Return.java
+++ src/crafting_interpreters/lox/Return.java
@@ -1,7 +1,7 @@
 package crafting_interpreters.lox;
 
 // This class is used by the lox interpreter to simulate a return in the interpreter.
-// It short cuts and clear the current execution stack with a throwable object,
+// It shortcuts and clear the current execution stack with a throwable object,
 // That object encapsulates the returned lox value.
 
 public class Return extends RuntimeException {
